0d56effa5bf150f65206d5f9e7e451074d371523
Rosa Jalinas <prof.jalinashgmail.com>
2023-07-22 11:07:17 -0600

    Se agragaron comentarios al codigo

--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
 ﻿
+//definicion de arreglo
 string[] names = { "Tom", "Dick", "Harry", "Mary", "Jay" };
+//este variable almacena los resultados de filtro
 IEnumerable<string> query = names
+//filtro para obtener nombre que contiene a
 .Where(n => n.Contains("a"))
+//ordenados por el tamaño de caracteres 
 .OrderBy(n => n.Length)
+//convertir los seleccionados a mayuscula
 .Select(n => n.ToUpper());
 foreach (var item in query)
     Console.Write($"{item} |");
